38375d8cf42736a079067e2d84a6727c2aedf176
Jhuthika <jhuthika.keerthi08@gmail.com>
2026-09-08 22:57:02 +0530

    Prepare Django app for deployment

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iy8wcoj)83kyr)9tgof2ma6#6u^q8-l960#hs1v4t!f^=9_8nb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
